6c7f1624a14041fedfbf9c8b14b7a90533d78f7b
Kevin <fwt0209@gmail.com>
2021-05-12 00:49:39 +0800

    modify slogan style

--- public/js/script.js
+++ public/js/script.js
@@ -52,7 +52,7 @@ function imagePathStringsToJSON(data) {
 
 async function getViewPointList(page = 1, keyword = "") {
     return new Promise((resolve, reject) => {
-        let url = new URL('http://52.68.89.158:3000/api/attractions');
+        let url = new URL('http://127.0.0.1:3000/api/attractions');
         url.searchParams.set('page', page);
         url.searchParams.set('keyword', keyword);
 
