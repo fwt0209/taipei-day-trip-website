ea07e7173eaa9d32f6bfd3338717c4521ee5b9bb
Kevin <fwt0209@gmail.com>
2021-05-29 01:24:40 +0800

    make slide dynamically generated

--- public/js/attraction.js
+++ public/js/attraction.js
@@ -1,27 +1,98 @@
 document.addEventListener("DOMContentLoaded", init)
+let slideIndex = 1;
 
 async function init() {
-    const attraction_id = getLastUrlPath(window.location.pathname)
+    const attraction_id = getAttractionId(window.location.pathname)
     const viewPoint = await getViewPoint(attraction_id)
     console.log(viewPoint)
+    createSlides(viewPoint.data.images)
+    showSlides(slideIndex);
+    document.addEventListener("keydown", (e) => {
+        if (e.keyCode === 39) plusSlides(1)
+    })
+    document.addEventListener("keydown", (e) => {
+        if (e.keyCode === 37) plusSlides(-1)
+    })
 }
 
-let slideIndex = 1;
-showSlides(slideIndex);
+let price = document.getElementById("price")
+let morningRadioBtn = document.getElementById("morning")
+morningRadioBtn.addEventListener("change", () => {
+    price.textContent = "新台幣2000元"
+})
+let afternoonRadioBtn = document.getElementById("afternoon")
+afternoonRadioBtn.addEventListener("change", () => {
+    price.textContent = "新台幣2500元"
+})
 
-// Next/previous controls
+
+function createSlides(images) {
+    let rawData = JSON.parse(images)
+    const totalSlides = rawData.length
+    let slideNumber = 0
+    let slideshow = document.getElementById("slideshow")
+    let docFrag = document.createDocumentFragment();
+    rawData.forEach(x => {
+        slideNumber++
+        let slide = document.createElement("div")
+        slide.classList.add("slides", "fade")
+        let slideImg = document.createElement("img")
+        slideImg.src = x
+        slide.appendChild(slideImg)
+        let slidePageText = document.createElement("div")
+        slidePageText.classList.add("numbertext")
+        slidePageText.textContent = `${slideNumber} / ${totalSlides}`
+        slide.appendChild(slidePageText)
+        docFrag.appendChild(slide)
+    });
+
+    slideNumber = 0
+    let pagination = document.createElement("div")
+    pagination.classList.add("pages")
+    rawData.forEach(x => {
+        slideNumber++
+        let dot = document.createElement("span")
+        dot.classList.add("dot")
+        dot.addEventListener("click", () => {
+            currentSlide(slideNumber)
+        })
+        pagination.appendChild(dot)
+    });
+    docFrag.appendChild(pagination)
+
+    let prev = document.createElement("a")
+    prev.classList.add("prev")
+    prev.appendChild(document.createTextNode("\u276E"))
+    prev.addEventListener("click", () => {
+        plusSlides(-1)
+    })
+    docFrag.appendChild(prev)
+
+    let next = document.createElement("a")
+    next.classList.add("next")
+    next.appendChild(document.createTextNode('\u276F'))
+    next.addEventListener("click", () => {
+        plusSlides(1)
+    })
+    docFrag.appendChild(next)
+
+
+    slideshow.appendChild(docFrag)
+}
+
+
+
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("mySlides");
+    let slides = document.getElementsByClassName("slides");
     let dots = document.getElementsByClassName("dot");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
@@ -35,7 +106,9 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
 }
 
-function getLastUrlPath(urlPath) {
+
+
+function getAttractionId(urlPath) {
     return urlPath.substring(urlPath.lastIndexOf("/") + 1)
 }
 
